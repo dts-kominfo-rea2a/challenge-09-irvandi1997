fb188666c9bf42b377e6f8c9ea52fe63c05e8813
irvandi1997 <106160897+irvandi1997@users.noreply.github.com>
2022-06-21 21:35:15 +0700

    tantangan react intro (src/components)

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,26 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ contacts }) => {
     return (
-        <></>
+        <div>
+            {
+                contacts.map((contact, i) => 
+                    <div className="contact" key={i}>
+                        <div style={{ flexGrow: 4 }}>
+                            <img src={contact.photo} alt="Monica" width="150px" style={{ borderRadius: "50%" }} />
+                        </div>
+                        <div style={{ flexGrow: 8 }}>
+                            <h4>{contact.name}</h4>
+                            <p>{contact.phone}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                    </div>
+                )
+            }
+        </div>
     )
 }
 
